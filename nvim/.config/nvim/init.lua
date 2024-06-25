673ea50966e7a82375f0641f304a930502cf3ef4
Ben Cutrell <bcutrell@apple-mbp.mynetworksettings.com>
2024-06-25 13:06:27 -0400

    adds rust-analyzer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -574,7 +574,7 @@ require('lazy').setup({
         gopls = {
           filetypes = { 'go' },
         },
-        -- rust_analyzer = {},
+        rust_analyzer = { 'rs' },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
