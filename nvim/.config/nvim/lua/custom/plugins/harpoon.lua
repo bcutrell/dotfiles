78f88776142a406f8a135b52a245d52d65cbb62a
Ben Cutrell <bcutrell@Apple-MacBook-Pro.local>
2024-07-25 10:00:27 -0400

    Edits harpoon config

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -5,18 +5,18 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup()
 
-    vim.keymap.set('n', '<m-h><m-m>', function()
+    vim.keymap.set('n', '<leader>A', function()
       harpoon:list():add()
-    end)
-    vim.keymap.set('n', '<m-h><m-l>', function()
+    end, { desc = 'harpoon add' })
+    vim.keymap.set('n', '<leader>H', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = 'harpoon toggle_quick_menu' })
 
     -- Set <space>1..<space>5 be my shortcuts to moving to the files
     for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-      vim.keymap.set('n', string.format('<space>%d', idx), function()
+      vim.keymap.set('n', string.format('<leader>%d', idx), function()
         harpoon:list():select(idx)
-      end)
+      end, { desc = string.format('harpoon shortcut %d', idx) })
     end
   end,
 }
