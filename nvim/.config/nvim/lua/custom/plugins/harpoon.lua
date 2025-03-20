5bda2b824314fbafc5ee72ac08c6e9e59bae2806
Ben Cutrell <bcutrell13@gmail.com>
2025-03-20 05:32:26 -0400

    Adds oil file manager

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,22 +1,39 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  config = function()
-    local harpoon = require 'harpoon'
-    harpoon:setup()
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    config = function()
+        local harpoon = require 'harpoon'
+        harpoon:setup()
 
-    vim.keymap.set('n', '<leader>A', function()
-      harpoon:list():add()
-    end, { desc = 'harpoon add' })
-    vim.keymap.set('n', '<leader>H', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'harpoon toggle_quick_menu' })
+        vim.keymap.set('n', '<leader>ha', function()
+            harpoon:list():append()
+        end, { desc = 'Harpoon: Add file' })
 
-    -- Set <space>1..<space>5 be my shortcuts to moving to the files
-    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-      vim.keymap.set('n', string.format('<leader>%d', idx), function()
-        harpoon:list():select(idx)
-      end, { desc = string.format('harpoon shortcut %d', idx) })
-    end
-  end,
+        vim.keymap.set('n', '<leader>he', function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = 'Harpoon: Toggle menu' })
+
+        vim.keymap.set('n', '<leader>hp', function()
+            harpoon:list():prev()
+        end, { desc = 'Harpoon: Go to previous file' })
+
+        vim.keymap.set('n', '<leader>hn', function()
+            harpoon:list():next()
+        end, { desc = 'Harpoon: Go to next file' })
+
+        vim.keymap.set('n', '<leader>hc', function()
+            harpoon:list():clear()
+        end, { desc = 'Harpoon: Clear all marks' })
+
+        vim.keymap.set('n', '<leader>hr', function()
+            harpoon:list():remove()
+        end, { desc = 'Harpoon: Remove current file' })
+
+        -- shortcuts
+        for i = 1, 3 do
+            vim.keymap.set('n', string.format('<leader>%d', i), function()
+                harpoon:list():select(i)
+            end, { desc = string.format('Harpoon: Jump to file %d', i) })
+        end
+    end,
 }
